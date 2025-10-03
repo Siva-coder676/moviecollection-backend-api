a9bee26a632080baf7d9787093c12f3411026df8
Siva-coder676 <sivaprajee@gmail.com>
2025-10-03 11:23:19 +0530

    new changes

--- middleware/send.mail.js
+++ middleware/send.mail.js
@@ -5,7 +5,7 @@ const nodemailer= require('nodemailer');
 const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
         pass: process.env.NODE_CODE_SENDING_EMAIL_PASSWORD
